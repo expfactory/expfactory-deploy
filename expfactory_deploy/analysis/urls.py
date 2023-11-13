b950b40a96b57b1f3c93e939bec8343cccc1ec36
Ross Blair <rosswilsonblair@gmail.com>
2023-11-13 22:01:22 +0000

    fix analysis urls. add detail to qa_by_sc

--- expfactory_deploy/analysis/urls.py
+++ expfactory_deploy/analysis/urls.py
@@ -3,16 +3,16 @@ from analysis import views
 
 app_name = "analysis"
 urlpatterns = [
-    path("qa_by_sc/<int:id>", view.qa_by_sc, name="qa-by-sc"),
+    path("qa_by_sc/<int:id>", views.qa_by_sc, name="qa-by-sc"),
     path(
         "trigger_qa_by_sc/<int:id>/",
-        view.trigger_qa_by_sc,
-        {"rerun": Flase},
+        views.trigger_qa_by_sc,
+        {"rerun": False},
         name="trigger-qa-by-sc",
     ),
     path(
         "trigger_qa_by_sc/<int:id>/rerun",
-        view.trigger_qa_by_sc,
+        views.trigger_qa_by_sc,
         {"rerun": True},
         name="trigger-qa-by-sc-rerun",
     ),
