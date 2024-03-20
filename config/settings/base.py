4721892c2c1d92e9a5e9f8a6b2ece1b9e35ed5ec
Ross Blair <rosswilsonblair@gmail.com>
2024-03-20 09:50:05 -0500

    use pacific time

--- config/settings/base.py
+++ config/settings/base.py
@@ -23,7 +23,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Los_Angeles"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -344,6 +344,7 @@ Q_CLUSTER = {
     'queue_limit': 500,
     'bulk': 10,
     'orm': 'default',
-    'poll': 4
+    'poll': 4,
+    'max_attempts': 3,
 }
 
