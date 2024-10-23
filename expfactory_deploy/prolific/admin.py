f462fb465de58947717f9860905fcc798fb84ffb
Ross Blair <rosswilsonblair@gmail.com>
2024-10-23 13:43:09 -0500

    show args in schedule admin list

--- expfactory_deploy/prolific/admin.py
+++ expfactory_deploy/prolific/admin.py
@@ -4,3 +4,22 @@ from django.apps import apps
 
 for model in apps.get_app_config("prolific").models.values():
     admin.site.register(model)
+
+from django_q import models as q_models
+from django_q import admin as q_admin
+
+admin.site.unregister([q_models.Schedule])
+@admin.register(q_models.Schedule)
+class ModedScheduleAdmin(q_admin.ScheduleAdmin):
+    list_display = (
+        "id",
+        "name",
+        "func",
+        "args",
+        "schedule_type",
+        "repeats",
+        "cluster",
+        "next_run",
+        "get_last_run",
+        "get_success",
+    )
